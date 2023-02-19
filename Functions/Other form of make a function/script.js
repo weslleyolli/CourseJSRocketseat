6c325c52b09ca6e9c97e6a0195232c6a9eb4a43b
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-02-19 18:31:35 -0300

    Other form of make a function

---
+++ Functions/Other form of make a function/script.js
@@ -0,0 +1,9 @@
+//function 
+
+function makeJuice(fruitOne, fruitTwo) {
+    return `juice of: ${fruitOne} ${fruitTwo}`
+}
+
+const glass = makeJuice('banana', 'strawberry')
+
+console.log(glass)
---
+++ Functions/Return value in the function/script.js
@@ -0,0 +1,14 @@
+//function anonymous
+const sum = function(numberOne, numberTwo) {
+    total = numberOne + numberTwo
+    return total  
+}
+
+let number1 = 27;
+let number2 = 33;
+
+sum(number1, number2)
+
+console.log(`the number one is ${number1} and the number two is ${number2}`)
+console.log(`the sum between the number is ${sum(number1, number2)}`)
+
