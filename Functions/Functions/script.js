85f30f00614fb5a45bad328a0336b5f984c62747
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-02-18 22:03:03 -0300

    functions

---
+++ Functions/Functions/script.js
@@ -0,0 +1,11 @@
+//create aplication of phrases
+
+
+function createPhrase() {
+    console.log('Study is very good')
+    console.log('patience and persistence')
+    console.log('proofreading is the mother of learning')
+}
+
+createPhrase()
+createPhrase()
