ee40323a47c8f43a33f8ed9bf5d2400c8e7c6e19
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-02-22 19:34:21 -0300

    separating strings

---
+++ Data manipulation/Separating strings/script.js
@@ -0,0 +1,7 @@
+//to separate a text that count spaces
+let phrase = "I want to live the love"
+//transforming in array
+let myArray = phrase.split(" ")
+//transforming in string separeted by _
+let phraseWithUnderScore  = myArray.join("_")
+console.log(phraseWithUnderScore)
