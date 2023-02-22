c83cd263c8ded3d8c7346b9c665df9bdca92f9cb
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-02-22 19:52:04 -0300

    Array with constructor and find word

---
+++ Data manipulation/Creating array with constructor/script.js
@@ -0,0 +1,6 @@
+// create array with constructor
+
+let myArray = new Array('a', 'b', 'c')
+let array = new Array(10) //10 positions empty
+
+console.log(myArray)
---
+++ Data manipulation/Finding word in phrases/script.js
@@ -0,0 +1,4 @@
+//Checking if the text count the word love
+
+let phrase = "I want to live th love"
+console.log(phrase.includes('love'))
