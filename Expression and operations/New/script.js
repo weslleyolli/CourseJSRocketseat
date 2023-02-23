c8de297128521a41044de7e3faffcd5259abe594
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-02-22 21:30:01 -0300

    expression and operations

---
+++ Expression and operations/New/script.js
@@ -0,0 +1,14 @@
+/*
+    New
+
+    * left-hand-side expression
+    * create a new object
+
+*/
+
+let name = new String('Weslley')
+name.surName = "Oliveira"
+
+let age = new Number(23)
+
+console.log(name.surName, age)
