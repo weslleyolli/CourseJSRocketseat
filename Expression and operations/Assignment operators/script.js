6437b2c8b84ff8d5d7cdc5d7e9b4e01bceef8aea
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-02-24 18:56:53 -0300

    Operators og assignment

---
+++ Expression and operations/Assignment operators/script.js
@@ -0,0 +1,20 @@
+// Operators of Assigment(atribução)
+
+let x
+
+//assigment
+x = 0
+//addition assignment
+x += 2
+//subtraction assignment
+x -= 1
+//multiplication assignment
+x *= 2
+//division assignment
+x /= 1
+// remainder
+x %= 2
+ console.log(x)
+ x = 2 
+ x **= 2
+ console.log(x)
