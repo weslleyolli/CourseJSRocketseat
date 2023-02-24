c1ab37ab091ac9add6747110bd8f65b2d51b2434
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-02-24 18:41:54 -0300

    logic operations

---
+++ Expression and operations/Operations aritmeticos/script.js
@@ -0,0 +1,30 @@
+//operations
+
+console.log(3.2 * 5.5)
+
+console.log(12 / 2)
+
+console.log(34 + 67)
+
+console.log(34 - 23)
+// rest
+let remainder
+
+remainder = 11 % 3
+console.log(remainder)
+
+//increment
+let increment
+increment = 0
+increment++
+
+console.log(increment)
+
+//decrement
+let decrement
+decrement = 1
+decrement--
+console.log(decrement)
+
+//exponential
+console.log(2 ** 3)
