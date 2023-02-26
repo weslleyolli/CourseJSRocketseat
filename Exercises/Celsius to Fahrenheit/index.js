7be9c2b7d99450b0f4f11f20b1464bb8bc5e03c2
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-02-25 22:15:26 -0300

    Exercise finished

--- Exercises/Celsius to Fahrenheit/index.js
+++ Exercises/Celsius to Fahrenheit/index.js
@@ -1,15 +1,28 @@
 const transformDegree = (degrees) => {
     const celsiusExists = degrees.toUpperCase().includes('C')
-    const farenheitExist = degrees.toUpperCase().includes('F')  
+    const fahrenheitExist = degrees.toUpperCase().includes('F')
 
-    if (!celsiusExists && !farenheitExist) {
+    if (!celsiusExists && !fahrenheitExist) {
         throw new Error('Invalid')
     }
+    //ideal flow
+    let updatedDegree = Number(degrees.toUpperCase().replace('F', ''))
+    let formula = (fahrenheit) => (fahrenheit - 32) * 5/9
+    let degreeSing = 'C'
 
+    //alternate flow
+    if (celsiusExists) {
+        updatedDegree = Number(degrees.toUpperCase().replace('C', ''))
+        formula = celsius => celsius * 9/5 + 32
+        degreeSing = 'F'
+    }
+
+    return formula(updatedDegree) + degreeSing
 }
 
 try {
-    transformDegree('50F')
+    console.log(transformDegree('50F'))
+    console.log(transformDegree('10C'))
 } catch (e) {
     console.log(e.message)
 }
