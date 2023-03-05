9fc1c1780b7837702c6ae72b5531afff4743dfe4
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-05 11:27:18 -0300

    Attribute

---
+++ Dom/attribute/script.js
@@ -0,0 +1,10 @@
+//attribute
+
+const header = document.querySelector('header')
+header.setAttribute('id', 'header')
+
+const headerID = document.querySelector('#header')
+
+console.log(headerID.getAttribute('class'))
+
+header.removeAttribute('class', 'one')
