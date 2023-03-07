452de1601064bb875e02093658504760e187fc06
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-07 17:59:48 -0300

    views and partials

--- nodeEjs/server.js
+++ nodeEjs/server.js
@@ -4,11 +4,11 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('pages/index');
 })
 
 app.get('/sobre/', (req, res) => {
-    res.render("about")
+    res.render("pages/about")
 })
 
 app.listen(8080)
