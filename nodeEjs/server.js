1bd0c8ca773580615623bf66b1c8f2d92d16706c
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-08 15:53:02 -0300

    course node EJS completed

--- nodeEjs/server.js
+++ nodeEjs/server.js
@@ -4,7 +4,31 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    res.render('pages/index');
+    const items = [
+        {   title: "D ",
+            message: "Application development"    
+        },
+        {   title: "E ",
+            message: "EJS use JavaScript to render application"    
+        },
+        {   title: "M ",
+            message: "Very ease to use"    
+        },
+        {   title: "A ",
+            message: "Application development"    
+        },
+        {   title: "I ",
+            message: "Install Ejs"    
+        },
+        {   title: "S ",
+            message: "Sintax simple development"    
+        }
+    ]
+    const subtitle = "EJS use JavaScript to render application"
+    res.render('pages/index', {
+        qualitys: items,
+        subtitle: subtitle,
+    });
 })
 
 app.get('/sobre/', (req, res) => {
